6b7946ff4b708b052bc2208757c43a2900ce44e0
watson <joshuatsn@gmail.com>
2014-12-22 21:45:06 +0530

    added init file dir structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImageUrlParse::Application.config.secret_key_base = '31a08dcaca91202e060ded6a9132e55a014be6f3a196468f72a6fd1a27f5f53628dc99ddc2fa559845effec58e996f167389835506d55a3e10268442b1df4609'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImageUrlParse::Application.config.session_store :cookie_store, key: '_image_url_parse_session'
